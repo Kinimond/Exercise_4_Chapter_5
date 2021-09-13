987d6e081021e29810cbc5f6f2ca4190d7eda790
Patrick Kinimond Hassø <89964910+Kinimond@users.noreply.github.com>
2021-09-13 18:05:57 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean yes = true;
+        boolean no = false;
+        int loVal = -999;
+        int hiVal = 999;
+        double grade = 87.5;
+        double amount = 50.0;
+        String hello = "world";
+
+        /*
+        yes == no || grade > amount           true
+        amount == 40.0 || 50.0                true
+        hiVal != loVal || loVal < 0           true
+        True || hello.length() > 0            true
+        hello.isEmpty() && yes                false
+        grade <= 100 && !false                true
+        !yes || no                            false
+        grade > 75 > amount                   true
+        amount <= hiVal && amount >= loVal    true
+        no && !no || yes && !yes              false
+        */
+
+
+
+
+
+
+    }
+}
